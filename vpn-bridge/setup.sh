d4a3e0e14e6abf97e70d3c5379641ae85998e174
mzpkdev <mateusz@mzpk.dev>
2025-05-18 20:31:23 +0200

    :tada:

--- vpn-bridge/setup.sh
+++ vpn-bridge/setup.sh
@@ -9,15 +9,15 @@ echo
 
 
 echo "Setting up server..."
-./vpncmd localhost /SERVER /CMD ServerPasswordSet "$ADMIN_PASSWORD"
+./vpnserver/vpncmd localhost /SERVER /CMD ServerPasswordSet "$ADMIN_PASSWORD"
 
 echo "Creating virtual hub..."
-./vpncmd localhost /SERVER /CMD HubCreate $VIRTUAL_HUB
-./vpncmd localhost /SERVER /HUB:$VIRTUAL_HUB /CMD PasswordSet "$ADMIN_PASSWORD"
+./vpnserver/vpncmd localhost /SERVER /CMD HubCreate $VIRTUAL_HUB
+./vpnserver/vpncmd localhost /SERVER /HUB:$VIRTUAL_HUB /CMD PasswordSet "$ADMIN_PASSWORD"
 
 echo "Setting up \"$BRIDGE_INTERFACE\" bridge..."
-./vpncmd localhost /SERVER /CMD BridgeCreate $VIRTUAL_HUB
-./vpncmd localhost /SERVER /CMD BridgeAdd $VIRTUAL_HUB $BRIDGE_INTERFACE
+./vpnserver/vpncmd localhost /SERVER /CMD BridgeCreate $VIRTUAL_HUB
+./vpnserver/vpncmd localhost /SERVER /CMD BridgeAdd $VIRTUAL_HUB $BRIDGE_INTERFACE
 
 
 echo "Your \"$VIRTUAL_HUB\" virtual hub is ready!"
